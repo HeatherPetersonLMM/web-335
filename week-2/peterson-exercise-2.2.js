f5d8c7a09641913ecb1bcfd3c9bce1895155de54
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-05-13 22:36:20 -0400

    adding js

---
+++ week-2/peterson-exercise-2.2.js
@@ -0,0 +1,41 @@
+
+/*
+============================================
+; Title:  NodeShopper ORD
+; Author: Professor Krasso 
+; Date:   13 May 2018
+; Modified By: Heather Peterson
+; Description: Exercise 2.2
+;===========================================
+*/ 
+
+
+//in customers
+{
+    "_id": 01;
+    "first_name": "Berkan",
+    "last_name": "Simsek",
+    "date_of_birth": "01/07/87",
+    "user_name": "BearS"
+}
+
+
+//in invoices
+{
+    "_id": 01;
+    "date_created": "03/08/2017",
+    "date_shipped": "03/18/2017",
+    "subtotal": 100.89,
+    "tax": 15.01,
+    "total": 115.90
+}
+
+
+
+//in line items
+{
+    "_id": 01;
+    "name": "Goodyear Duo Tire Set",
+    "price": 100.89,
+    "quantity": 1
+}
