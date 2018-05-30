75300e1bf9dfc344a5ed03b6363f11832d2dfbea
Heather Peterson <heatherpeterson@Heathers-MacBook-Pro.local>
2018-05-29 21:25:09 -0400

    added mongoorc

---
+++ week-5/mongorc.js
@@ -0,0 +1,6 @@
+//mongorc.js
+
+var compliment = ["smart", "strong", "motivated", "ready", "like Aloy!"];
+var index = Math.floor(Math.random()*5);
+
+print("Hello Heather, keep being " +compliment[index]+" and " +compliment[index]+" today!");
