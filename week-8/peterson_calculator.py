88aa9e4a7b612c30e96ea90141793765ccd6fb2f
Heather <hpeterson@my.bellevue.edu>
2018-06-24 14:11:34 -0400

    added exercise 8.3

---
+++ week-8/peterson_calculator.py
@@ -0,0 +1,18 @@
+def myName():
+    return "Heather Peterson"
+
+def add(a,b):
+    return a, "+", b, "=", a + b
+
+def sub(a,b):
+    return b, "-", a, "=", b - a
+
+def div(a,b):
+    return a, "/", b, "=", a / b
+
+print(myName())
+print(add(1,2))
+print(sub(4,1))
+print(div(8,2))
+
+
